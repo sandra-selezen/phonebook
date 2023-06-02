d16373359508b3af600275d925bc07bf579a8266
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-06-02 19:53:11 +0000

    Deploying to gh-pages from @ sandra-selezen/goit-react-hw-08-phonebook@93a5b964aa9dc51edeba19f64e945342e7be5044 🚀

---
+++ static/js/266.4cab2174.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[266],{1266:function(e,o,n){n.r(o);var t=n(4087),u=(n(2791),n(184));o.default=function(){return(0,u.jsx)(t.xu,{children:"Home Page"})}}}]);
+//# sourceMappingURL=266.4cab2174.chunk.js.map
