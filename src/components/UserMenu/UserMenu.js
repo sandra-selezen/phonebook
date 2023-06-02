ef61765fe4f164488bffab37a78be5498fce1d6b
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-06-02 18:17:28 +0300

    Update UserMenu.js

---
+++ src/components/UserMenu/UserMenu.js
@@ -0,0 +1,11 @@
+import { Box, Button, Text } from '@chakra-ui/react';
+import React from 'react';
+
+export const UserMenu = () => {
+  return (
+    <Box>
+      <Text>Username</Text>
+      <Button>Logout</Button>
+    </Box>
+  )
+}
