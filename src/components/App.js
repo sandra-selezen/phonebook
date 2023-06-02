62fbd4eff8df2de1b29a359268ea7a15942261b2
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-06-02 19:26:43 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
+// import { lazy } from "react";
+// import { Routes, Route } from "react-router-dom";
 import { Footer } from "./Footer/Footer";
 import { Header } from "./Header/Header";
+
+// const HomePage = lazy(() => import("../pages/Home"));
+// const ContactsPage = lazy(() => import("../pages/Contacts"));
+// const SignUpPage = lazy(() => import("../pages/SignUp"));
+// const LoginPage = lazy(() => import("../pages/Login"));
 
 export const App = () => {
   return (
@@ -9,5 +16,14 @@ export const App = () => {
       Welcome to your Phonebook
       <Footer />
     </div>
+    // <Routes>
+    //   <Route path="/" element={<HomePage />}>
+    //     <Route index element={<HomePage />} />
+    //     <Route path="/contacts" element={<ContactsPage />} />
+    //     <Route path="/signup" element={<SignUpPage />} />
+    //     <Route path="/login" element={<LoginPage />} />
+    //     <Route path="*" element={<HomePage />} />
+    //   </Route>
+    // </Routes>
   );
 };
