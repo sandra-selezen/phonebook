cd39dc69294fbcd6c305c93a7c5d8da03143fbf2
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-06-02 18:08:47 +0300

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,8 @@ export const Footer = () => {
   return (
     <Box as='footer' padding={'24px 32px'} textAlign={'center'}>
       <Text>
-        &#169; 2023 | All Rights Reserved | Developed with 💙💛 by <Link href='https://www.linkedin.com/in/aleksandra-selezen/' isExternal>Sandra Selezen</Link>
+        &#169; 2023 | All Rights Reserved | Developed with 💙💛 by{" "}
+        <Link href='https://www.linkedin.com/in/aleksandra-selezen/' isExternal>Sandra Selezen</Link>
       </Text>
     </Box>
   )
