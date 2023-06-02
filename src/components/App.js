709717254c6c0009397205d966edad7c5d475536
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-06-02 18:52:57 +0300

    Adds SignUpForm component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Footer } from "./Footer/Footer";
 import { Header } from "./Header/Header";
+import { SignUpForm } from './SignUpForm/SignUpForm';
 
 export const App = () => {
   return (
     <div>
       <Header />
+      <SignUpForm />
       Welcome to your Phonebook
       <Footer />
     </div>
---
+++ src/components/SignUpForm/SignUpForm.js
@@ -0,0 +1,46 @@
+import { Button, FormControl, FormLabel, Input } from '@chakra-ui/react';
+import { Field, Formik, Form } from 'formik';
+import React from 'react';
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+  email: yup.string().email().required(),
+  password: yup.string().required(),
+})
+
+const initialValues = {
+  name: "",
+  email: "",
+  password: "",
+}
+
+export const SignUpForm = () => {
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={schema}
+      onSubmit={(values, actions) => {
+        alert(JSON.stringify(values, null, 2));
+        actions.resetForm();
+      }}
+    >
+      {({ handleSubmit }) => (
+        <Form onSubmit={handleSubmit}>
+          <FormControl isRequired>
+            <FormLabel htmlFor='name'>Full name</FormLabel>
+            <Field as={Input} id='name' name='name' type='text' placeholder='Enter your full name' />
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel htmlFor='email'>Email</FormLabel>
+            <Field as={Input} id='email' name='email' type='email' placeholder='Enter email' />
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel htmlFor='password'>Password</FormLabel>
+            <Field as={Input} id='password' name='password' type='password' placeholder='Enter password' />
+          </FormControl>
+          <Button type='submit'>Sign Up</Button>
+        </Form>
+      )}
+    </Formik>
+  )
+}
