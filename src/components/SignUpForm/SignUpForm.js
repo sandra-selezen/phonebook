1b3acb610d1218db7a93ea59188b0c6c43b5fa2b
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-06-03 15:18:23 +0300

    Update SignUpForm.js
    Adds icons

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -1,7 +1,8 @@
-import { Button, FormControl, FormLabel, Input } from '@chakra-ui/react';
+import { Button, FormControl, FormLabel, Input, Icon } from '@chakra-ui/react';
 import { Field, Formik, Form } from 'formik';
 import React from 'react';
 import * as yup from 'yup';
+import { RiUserFill, RiMailFill, RiLock2Fill, RiLoginBoxFill } from "react-icons/ri";
 
 const schema = yup.object().shape({
   name: yup.string().required(),
@@ -28,18 +29,18 @@ export const SignUpForm = () => {
       {({ handleSubmit }) => (
         <Form onSubmit={handleSubmit}>
           <FormControl isRequired>
-            <FormLabel htmlFor='name'>Full name</FormLabel>
+            <FormLabel htmlFor='name'><Icon as={RiUserFill} />Full name</FormLabel>
             <Field as={Input} id='name' name='name' type='text' placeholder='Enter your full name' />
           </FormControl>
           <FormControl isRequired>
-            <FormLabel htmlFor='email'>Email</FormLabel>
+            <FormLabel htmlFor='email'><Icon as={RiMailFill} />Email</FormLabel>
             <Field as={Input} id='email' name='email' type='email' placeholder='Enter email' />
           </FormControl>
           <FormControl isRequired>
-            <FormLabel htmlFor='password'>Password</FormLabel>
+            <FormLabel htmlFor='password'><Icon as={RiLock2Fill} />Password</FormLabel>
             <Field as={Input} id='password' name='password' type='password' placeholder='Enter password' />
           </FormControl>
-          <Button type='submit'>Sign Up</Button>
+          <Button type='submit'><Icon as={RiLoginBoxFill} />Sign Up</Button>
         </Form>
       )}
     </Formik>
