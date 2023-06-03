abd026a3ce26957bcd51e78517efe07e4f51f341
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-06-03 15:09:11 +0300

    Update ContactForm.js
    Adds icons

--- src/components/ContactsForm/ContactForm.js
+++ src/components/ContactsForm/ContactForm.js
@@ -1,7 +1,8 @@
-import { Button, FormControl, FormLabel, Input } from '@chakra-ui/react';
+import { Button, FormControl, FormLabel, Input, Icon } from '@chakra-ui/react';
 import { Formik, Form, Field } from 'formik';
 import React from 'react';
 import * as yup from 'yup';
+import { RiPhoneFill, RiUserFill, RiUserAddFill } from "react-icons/ri";
 
 const schema = yup.object().shape({
   name: yup.string().required(),
@@ -26,14 +27,14 @@ export const ContactForm = () => {
       {({ handleSubmit }) => (
         <Form onSubmit={handleSubmit}>
           <FormControl isRequired>
-            <FormLabel htmlFor='name'>Full name</FormLabel>
+            <FormLabel htmlFor='name'><Icon as={RiUserFill} />Name</FormLabel>
             <Field as={Input} id='name' name='name' type='text' placeholder='Enter contact name' />
           </FormControl>
           <FormControl isRequired>
-            <FormLabel htmlFor='number'>Number</FormLabel>
+            <FormLabel htmlFor='number'><Icon as={RiPhoneFill} />Number</FormLabel>
             <Field as={Input} id='number' name='number' type='tel' placeholder='Enter contact phone number' />
           </FormControl>
-          <Button type='submit'>Sign Up</Button>
+          <Button type='submit' aria-label='Add contact'><Icon as={RiUserAddFill} /></Button>
         </Form>
       )}
     </Formik>
