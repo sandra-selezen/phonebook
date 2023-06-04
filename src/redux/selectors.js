d7ad1ff266316c49c6c87914274e914eaea3167f
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-06-04 17:18:15 +0300

    Adds filterSlice

---
+++ src/redux/selectors.js
@@ -0,0 +1,21 @@
+import { createSelector } from '@reduxjs/toolkit';
+
+export const selectContacts = state => state.contacts.items;
+export const selectIsLoading = state => state.contacts.isLoading;
+export const selectError = state => state.contacts.error;
+export const selectFilter = state => state.filter;
+
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    if (!filter) {
+      return contacts;
+    }
+    const normalizedFilter = filter.toLowerCase();
+    const filteredContacts = contacts.filter(({ name, number }) =>
+        name.toLowerCase().trim().includes(normalizedFilter) ||
+        number.trim().includes(normalizedFilter)
+    );
+    return filteredContacts;
+  }
+);
