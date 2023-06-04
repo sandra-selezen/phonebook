4708ee09a2eb07182024a169e9a7b711b9e994a1
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-06-04 21:41:24 +0300

    Update Filter.js

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -2,7 +2,7 @@ import { RiUserSearchFill } from "react-icons/ri";
 import { useDispatch } from 'react-redux';
 import { useFilter } from "hooks/hooks";
 import { setFilter } from 'redux/filterSlice';
-import { Input, Text } from "@chakra-ui/react";
+import { Input, Text, Icon } from "@chakra-ui/react";
 
 export const Filter = () => {
   const filter = useFilter();
@@ -14,7 +14,7 @@ export const Filter = () => {
 
   return (
     <>
-      <Text><RiUserSearchFill /> Find contact by name </Text>
+      <Text><Icon as={RiUserSearchFill} /> Find contact by name </Text>
       <Input value={filter} onChange={handleFilter} autoComplete="off" />
     </>
   )
