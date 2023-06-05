afd4e1e59144919ae95ebc571a84d7838639aa44
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-06-05 13:21:41 +0300

    Adds authSlice

--- src/hooks/hooks.js
+++ src/hooks/hooks.js
@@ -1,5 +1,18 @@
 import { useSelector } from 'react-redux';
+import { selectIsLoggedIn, selectIsRefreshing, selectUser } from 'redux/auth/selectors';
 import { selectContacts, selectFilter, selectError, selectFilteredContacts, selectIsLoading } from 'redux/selectors';
+
+export const useAuth = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isRefreshing = useSelector(selectIsRefreshing);
+  const user = useSelector(selectUser);
+
+  return {
+    isLoggedIn,
+    isRefreshing,
+    user,
+  };
+};
 
 export const useContacts = () => useSelector(selectContacts);
 export const useFilter = () => useSelector(selectFilter);
