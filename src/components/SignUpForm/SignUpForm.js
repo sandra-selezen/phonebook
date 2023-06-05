543c9bc0fd6c0e01d9f2b9bbde34aeedcc588af7
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-06-05 14:36:55 +0300

    Adds registration

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -3,6 +3,8 @@ import { Field, Formik, Form } from 'formik';
 import React from 'react';
 import * as yup from 'yup';
 import { RiUserFill, RiMailFill, RiLock2Fill, RiLoginBoxFill } from "react-icons/ri";
+import { useDispatch } from 'react-redux';
+import { register } from 'redux/auth/operations';
 
 const schema = yup.object().shape({
   name: yup.string().required(),
@@ -17,32 +19,42 @@ const initialValues = {
 };
 
 export const SignUpForm = () => {
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (values, actions) => {
+    console.log(values, actions);
+    dispatch(
+      register({
+        name: values.name,
+        email: values.email,
+        password: values.password,
+      })
+    );
+    actions.resetForm();
+  }
+
   return (
     <Formik
       initialValues={initialValues}
       validationSchema={schema}
-      onSubmit={(values, actions) => {
-        alert(JSON.stringify(values, null, 2));
-        actions.resetForm();
-      }}
+      onSubmit={handleSubmit}
     >
-      {({ handleSubmit }) => (
-        <Form onSubmit={handleSubmit}>
-          <FormControl isRequired>
-            <FormLabel htmlFor='name'><Icon as={RiUserFill} />Full name</FormLabel>
-            <Field as={Input} id='name' name='name' type='text' placeholder='Enter your full name' />
-          </FormControl>
-          <FormControl isRequired>
-            <FormLabel htmlFor='email'><Icon as={RiMailFill} />Email</FormLabel>
-            <Field as={Input} id='email' name='email' type='email' placeholder='Enter email' />
-          </FormControl>
-          <FormControl isRequired>
-            <FormLabel htmlFor='password'><Icon as={RiLock2Fill} />Password</FormLabel>
-            <Field as={Input} id='password' name='password' type='password' placeholder='Enter password' />
-          </FormControl>
-          <Button type='submit'><Icon as={RiLoginBoxFill} />Sign Up</Button>
-        </Form>
-      )}
+      <Form>
+        <FormControl isRequired>
+          <FormLabel htmlFor='name'><Icon as={RiUserFill} />Full name</FormLabel>
+          <Field as={Input} id='name' name='name' type='text' placeholder='Enter your full name' />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel htmlFor='email'><Icon as={RiMailFill} />Email</FormLabel>
+          <Field as={Input} id='email' name='email' type='email' placeholder='Enter email' />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel htmlFor='password'><Icon as={RiLock2Fill} />Password</FormLabel>
+          <Field as={Input} id='password' name='password' type='password' placeholder='Enter password' />
+        </FormControl>
+        <Button type='submit'><Icon as={RiLoginBoxFill} />Sign Up</Button>
+      </Form>
     </Formik>
   )
 };
