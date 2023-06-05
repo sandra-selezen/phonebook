def6062f0cb9d20b40d0353775dc80fa7705fa11
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-06-05 22:51:31 +0300

    Adds bg image

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,18 +4,19 @@ import { Footer } from './Footer/Footer';
 import { Outlet } from 'react-router-dom';
 import { Box } from '@chakra-ui/react';
 import { Toaster } from 'react-hot-toast';
+import bgImage from '../images/login-bg.png';
 
 export const Layout = () => {
   return (
-    <>
+    <Box>
       <Header />
-      <Box as='main'>
+      <Box as='main' bgImage={bgImage} bgPosition="center" bgRepeat="no-repeat" objectFit={'cover'} objectPosition={'center'}>
         <Suspense fallback={<div>Loading...</div>}>
           <Outlet />
         </Suspense>
       </Box>
       <Footer />
       <Toaster position="top-center" />
-    </>
+    </Box>
   )
 };
