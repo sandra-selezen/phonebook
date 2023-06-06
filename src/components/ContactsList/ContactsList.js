6bc164ea22cc04fef43641f372f1421bfe1d48f7
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-06-06 13:20:16 +0300

    Adds count of contacts

--- src/components/ContactsList/ContactsList.js
+++ src/components/ContactsList/ContactsList.js
@@ -1,29 +1,18 @@
 import { useDispatch } from 'react-redux';
-import { deleteContact, fetchContacts } from 'redux/operations';
-import { VStack, StackDivider, Text, Box, Button, Flex } from '@chakra-ui/react';
-import { useError, useFilteredContacts, useIsLoading } from 'hooks/hooks';
-import React, { useEffect } from 'react';
+import { deleteContact } from 'redux/operations';
+import { VStack, StackDivider, Text, Box, Button, Flex, Spinner } from '@chakra-ui/react';
+import React from 'react';
 import { RiUserUnfollowFill } from 'react-icons/ri';
 
-export const ContactList = () => {
-
-  const contacts = useFilteredContacts();
-  const isLoading = useIsLoading();
-  const error = useError();
+export const ContactList = ({ contacts, isLoading, error }) => {
 
   const dispatch = useDispatch();
   
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
-
   const onDeleteContact = (contactId) => dispatch(deleteContact(contactId));
 
   return (
     <>
-      {!contacts.length && !error && !isLoading && (
-        <Box><Text>Your Phonebook is empty</Text></Box>
-      )}
+      {isLoading && <Spinner />}
       {!error && !isLoading && contacts?.length > 0 && (
         <VStack spacing={4} divider={<StackDivider borderColor={'white'} />}>
           {contacts.map(contact => (
--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,5 +1,8 @@
 import {
+  VisuallyHidden,
   Heading,
+  Box,
+  Text,
   Button,
   Modal,
   ModalOverlay,
@@ -9,19 +12,40 @@ import {
   ModalCloseButton,
   useDisclosure
 } from '@chakra-ui/react';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchContacts } from 'redux/operations';
+import { useError, useFilteredContacts, useIsLoading } from 'hooks/hooks';
+
 import { ContactForm } from 'components/ContactsForm/ContactForm';
 import { ContactList } from 'components/ContactsList/ContactsList';
 import { CommonContainer } from 'components/Container/CommonContainer';
 import { Filter } from 'components/Filter/Filter';
 
 const Contacts = () => {
+
+  const contacts = useFilteredContacts();
+  const isLoading = useIsLoading();
+  const error = useError();
+  const contactsCount = contacts.length;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
   
   const { isOpen, onOpen, onClose } = useDisclosure();
   
   return (
     <CommonContainer>
-      <Heading>Contacts Page</Heading>
+      <VisuallyHidden><Heading>Contacts Page</Heading></VisuallyHidden>
+      {!contacts.length && !error && !isLoading && (
+        <Box><Text>Your Phonebook is empty</Text></Box>
+      )}
+      {contactsCount > 0 && (
+        <Box><Text>You have {contactsCount === 1 ? `${contactsCount} contact` : `${contactsCount} contacts`} in Phonebook</Text></Box>
+      )}
       <Button onClick={onOpen}>Add new contact</Button>
       <Filter />
       <Modal isOpen={isOpen} onClose={onClose}>
@@ -34,7 +58,7 @@ const Contacts = () => {
           </ModalBody>
         </ModalContent>
       </Modal>
-      <ContactList />
+      <ContactList contacts={contacts} isLoading={isLoading} error={error} />
     </CommonContainer>
   )
 };
