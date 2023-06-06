2fe1664055e4d3a491358fe79e26679880fbadea
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-06-06 19:05:20 +0300

    Update ContactForm.js

--- src/components/ContactsForm/ContactForm.js
+++ src/components/ContactsForm/ContactForm.js
@@ -50,11 +50,11 @@ export const ContactForm = () => {
     >
       <Form>
         <FormControl isRequired marginBottom={'12px'}>
-          <FormLabel htmlFor='name'><Icon as={RiUserFill} />Name</FormLabel>
+          <FormLabel htmlFor='name'><Icon as={RiUserFill} mr={'8px'} />Name</FormLabel>
           <Field as={Input} id='name' name='name' type='text' placeholder='Enter contact name' />
         </FormControl>
         <FormControl isRequired marginBottom={'12px'}>
-          <FormLabel htmlFor='number'><Icon as={RiPhoneFill} />Number</FormLabel>
+          <FormLabel htmlFor='number'><Icon as={RiPhoneFill} mr={'8px'} />Number</FormLabel>
           <Field as={Input} id='number' name='number' type='tel' placeholder='Enter contact phone number' />
         </FormControl>
         <Button type='submit'><Icon as={RiUserAddFill} />Add contact</Button>
