f672774b87ffe9524acbe0f2754a9fbcb62fb785
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-06-07 16:08:57 +0300

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -25,8 +25,7 @@ const changeContactFulfilledReduser = (state, action) => {
   const index = state.items.findIndex(
     contact => contact.id === action.payload.id
   );
-  // state.items.splice(index, 1, action.payload);
-  state.items[index] = action.payload;
+  state.items.splice(index, 1, action.payload);
 };
 const deleteContactFulfilledReducer = (state, action) => {
   state.isLoading = false;
